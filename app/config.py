3b89107354cf1cd01998a4cde67f4ff17fbaa2dc
vantage-ola <vantageola4@gmail.com>
2024-12-11 04:09:55 +0100

    mv to app/ + config- classes + extensions

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from decouple import config
+
+class Development:
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class Staging:
+    SECRET_KEY = config('TEST_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI =config('TEST_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class Production:
+    SECRET_KEY = config('PROD_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config('PROD_DATABASE_URI')
