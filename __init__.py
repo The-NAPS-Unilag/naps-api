10e2346a6db8cf1602a55f496bfd460f154afa5a
vantage-ola <vantageola4@gmail.com>
2024-12-11 04:10:36 +0100

    init app

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from app.config import Development, Staging, Production
+from app.extensions import db, ma
+
+from decouple import config
+
+def create_app():
+    app = Flask(__name__)
+
+    environment = config('FLASK_ENV', default='development').lower()
+
+    if environment == 'production':
+        app.config.from_object(Production)
+    elif environment == 'staging':
+        app.config.from_object(Staging)
+    else:  # defaults to development
+        app.config.from_object(Development)
+
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    return app
