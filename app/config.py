675e0ac1c3f5be48606ef9a0d0bc2c8286906397
vantage-ola <vantageola4@gmail.com>
2024-12-15 19:37:49 +0100

    autopep8 linter

--- app/config.py
+++ app/config.py
@@ -1,14 +1,17 @@
 from decouple import config
 
+
 class Development:
     SECRET_KEY = config('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = config('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+
 class Staging:
     SECRET_KEY = config('TEST_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI =config('TEST_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = config('TEST_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 
 class Production:
     SECRET_KEY = config('PROD_SECRET_KEY')
