ffe21428934fd0b4d41e96036d35f1c39215b1e0
vantage-ola <vantageola4@gmail.com>
2024-12-18 17:40:03 +0100

    tweak config class

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,19 @@
 from flask import Flask
 from app.config import Development, Staging, Production
-from app.extensions import db, ma, migrate
+from app.extensions import db, ma, migrate, jwt
 from app.routes.user_routes import user_bp
 from decouple import config
 
 
-def create_app():
+def create_app(config_class='app.config.Development'):
     app = Flask(__name__)
 
-    environment = config('FLASK_ENV', default='development').lower()
-
-    if environment == 'production':
-        app.config.from_object(Production)
-    elif environment == 'staging':
-        app.config.from_object(Staging)
-    else:  # defaults to development
-        app.config.from_object(Development)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     ma.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     app.register_blueprint(user_bp, url_prefix='/api')
 
