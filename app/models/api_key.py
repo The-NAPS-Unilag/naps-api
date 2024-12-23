5a3bdb351514d1ccf7d13c2f0d11bca42cf1d784
vantage-ola <vantageola4@gmail.com>
2024-12-23 15:26:27 +0100

    fix key not duplicating

--- app/models/api_key.py
+++ app/models/api_key.py
@@ -3,5 +3,5 @@ import secrets
 
 class APIKey(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(128), unique=True, nullable=False, default=secrets.token_hex(64))
+    key = db.Column(db.String(128), unique=True, nullable=False, default=lambda: secrets.token_hex(64))
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
