e95be750421d686947fde3ac4ab0a813a98cdc59
vantage-ola <vantageola4@gmail.com>
2025-01-27 22:39:57 +0100

    decorator docs

--- app/decorators/admin_decorator.py
+++ app/decorators/admin_decorator.py
@@ -5,6 +5,24 @@ from flask_jwt_extended import get_jwt_identity
 
 # function to allow only admin to generate api_key
 def admin_required(fn):
+    """
+    Decorator to restrict access to admin users only.
+
+    This decorator checks if the current user is an admin. If the user is not found
+    or is not an admin, it returns an appropriate error message and status code.
+    Otherwise, it allows the wrapped function to execute.
+
+    Args:
+        fn (function): The function to be wrapped by the decorator.
+
+    Returns:
+        function: The wrapped function with admin access control.
+
+    Example:
+        @admin_required
+        def generate_api_key():
+            # Function implementation
+    """
     @wraps(fn)
     def wrapper(*args, **kwargs):
         user_id = get_jwt_identity()
--- app/decorators/api_decorator.py
+++ app/decorators/api_decorator.py
@@ -3,6 +3,24 @@ from flask import request, jsonify
 from app.models.api_key import APIKey
 
 def api_key_required(fn):
+    """
+    Decorator to restrict access to endpoints requiring an API key.
+
+    This decorator checks if the request contains a valid API key in the headers.
+    If the API key is missing or invalid, it returns an appropriate error message
+    and status code. Otherwise, it allows the wrapped function to execute.
+
+    Args:
+        fn (function): The function to be wrapped by the decorator.
+
+    Returns:
+        function: The wrapped function with API key access control.
+
+    Example:
+        @api_key_required
+        def some_protected_route():
+            # Function implementation
+    """
     @wraps(fn)
     def wrapper(*args, **kwargs):
         api_key = request.headers.get('x-api-key')
